c5a9443f266ee64a518b60ab5fcc41bf85c49f94
victorioanoviciu-bit <victorioanoviciu@gmail.com>
2026-03-05 12:12:11 +0000

    Update comments and variable declarations in index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-// comentariu in js (cu // sau /** pt paragraf): var, let, const sant cele 3 key-worduri mai utilizate 
+// comentariu in js (cu // sau /** pt paragraf): var, let, const sant cele 3 key-worduri mai utilizate //
 /** declaram, initializam, dam o valoare (= atribuie o valoare unei variabile), deci am declarat variabila ´var´, 
 am initializat-o ´=´, am dat o valoare "Victor" **/
-// console.log("Codul a fost executat");
+// console.log("Codul a fost executat");//
 var myName ="Victor"
 let secondName ="Ioan";
 const age;
