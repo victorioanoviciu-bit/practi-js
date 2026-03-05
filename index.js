f3e74254f84bedbe61202190d38eca1d02399ec7
victorioanoviciu-bit <victorioanoviciu@gmail.com>
2026-03-05 12:06:01 +0000

    Fix variable declarations in index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // comentariu in js (cu // sau /** pt paragraf): var, let, const sant cele 3 key-worduri mai utilizate 
 console.log("Codul a fost executat");
-var: myName ="Victor"
+var myName ="Victor"
 /** declaram, initializam, dam o valoare (= atribuie o valoare unei variabile), deci am declarat variabila ´var´, 
 am initializat-o ´=´, am dat o valoare "Victor" **/
-let: secondName ="Ioan";
+let secondName ="Ioan";
 const age;
