d8440d6a8385168be2e4f8d795b6209742dec59f
victorioanoviciu-bit <victorioanoviciu@gmail.com>
2026-03-06 10:51:13 +0000

    1

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 // comentariu in js (cu // sau /** pt paragraf): var, let, const sant cele 3 key-worduri mai utilizate //
 /** declaram, initializam, dam o valoare (= atribuie o valoare unei variabile), deci am declarat variabila ´var´, 
-am initializat-o ´=´, am dat o valoare "Victor" **/
+am initializat-o "=", am dat o valoare "Victor" **/
 // console.log("Codul a fost executat");//
 var myName ="Victor"
 let secondName ="Ioan";
 const age = 30;
+// operatori:
+//aritmetici + - / * ++ --
+const sum = 1+1; console.log(sum);
+const difference = 2-1; console.log(difference);
+const division = 10/5; console.log(division);
+let multiplication = 10*5; console.log(multiplication);
+
+multiplication++;
+console.log(multiplication);
+
+multiplication--;
+console.log(multiplication);
