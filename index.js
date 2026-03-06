c153c0082ad8573ef3497e10afae8ad426d1ea0d
victorioanoviciu-bit <victorioanoviciu@gmail.com>
2026-03-06 14:09:52 +0000

    1

--- index.js
+++ index.js
@@ -5,3 +5,27 @@ am initializat-o ´=´, am dat o valoare "Victor" **/
 var myName ="Victor"
 let secondName ="Ioan";
 const age = 30;
+
+/* categorii: permisive si obiective */
+/* var - permisiv, poate fi redeclarat, poate fi reasignat, are scope de functie
+let - permisiv, nu poate fi redeclarat, poate fi reasignat, are scope de bloc
+const - obiectiv, nu poate fi redeclarat, nu poate fi reasignat, are scope de bloc */
+
+/* permisive */
+/* boolean */
+let boolean = true;
+
+/* null */
+let n = null;
+
+/* undefined */
+let undf = undefined;
+
+/* Number */
+let num = 123;
+
+/* string */
+let str = "Acesta este un string";
+
+console.log(str);
+console.log(typeof(str));
